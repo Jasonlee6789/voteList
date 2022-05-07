ef1356143386915c94ee6baf47ad521656d3f797
Jeff <li.jing@smartcompany.jp>
2022-05-07 10:50:24 +0900

    use Mongodb to add database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,16 @@
 const Express = require("express");
+const setupDatabase = require("./setup/database");
 const setupMiddleware = require("./setup/middleware");
+
 const app = Express();
+
 setupMiddleware(app);
 
-app.listen(4000, () => {
-  console.log("LIJING Server started on port 4000");
-});
+setupDatabase()
+  .then((client) => {
+    console.log(client);
+    app.listen(4000, () => {
+      console.log("LIJING Server started on port 4000");
+    });
+  })
+  .catch(console.error);
